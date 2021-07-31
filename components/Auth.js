0059f8cb5398d58a247be9e9071b396d82ec2d7b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-07-31 21:54:49 +0900

    インポートのパスを修正

--- components/Auth.js
+++ components/Auth.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useRouter } from "next/dist/client/router";
+import { useRouter } from "next/router";
 import { Cookie } from "universal-cookie";
 import { LockClosedIcon } from "@heroicons/react/solid";
 
