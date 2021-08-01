f09245032f58b7a81926ac67c298d45b555bb0a2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-01 20:58:30 +0900

    blog-pageにrestapiを追加

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,9 +1,16 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 
+import { getAllPostsData } from "../lib/posts";
+import Post from "../components/Post";
+
 export default function BlogPage({ filteredPosts }) {
   return (
     <Layout title="Blog page">
+      <ul>
+        {filteredPosts &&
+          filteredPosts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
       <Link href="/main-page" passHref>
         <div className="flex cursor-pointer mt-12">
           <svg
@@ -25,4 +32,11 @@ export default function BlogPage({ filteredPosts }) {
       </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const filteredPosts = await getAllPostsData();
+  return {
+    props: { filteredPosts },
+  };
 }
