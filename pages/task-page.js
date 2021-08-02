d60eec71f47f0c3dd31340ec71ed25c1f563c7f7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-02 22:49:06 +0900

    task-pageをuseSWR,ISR,SSGを使うよう修正

--- pages/task-page.js
+++ pages/task-page.js
@@ -1,9 +1,31 @@
-import Layout from "../components/Layout";
+import { useEffect } from "react";
 import Link from "next/link";
+import useSWR from "swr";
 
-export default function TaskPage({ filteredPosts }) {
+import Layout from "../components/Layout";
+import { getAllTasksData } from "../lib/tasks";
+import Task from "../components/Task";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
+
+export default function TaskPage({ staticfilteredTasks }) {
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    initialData: staticfilteredTasks,
+  });
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+  useEffect(() => {
+    mutate();
+  }, []);
+
   return (
     <Layout title="Task page">
+      <ul>
+        {filteredTasks &&
+          filteredTasks.map((task) => <Task key={task.id} task={task} />)}
+      </ul>
       <Link href="/main-page" passHref>
         <div className="flex cursor-pointer mt-12">
           <svg
@@ -25,4 +47,12 @@ export default function TaskPage({ filteredPosts }) {
       </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const staticfilteredTasks = await getAllTasksData();
+  return {
+    props: { staticfilteredTasks },
+    revalidate: 3,
+  };
 }
