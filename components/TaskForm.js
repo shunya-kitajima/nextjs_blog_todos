49a495cfcd3512abb7e0348176b946c9c7c0d5f9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-03 02:57:04 +0900

    元に戻す

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -54,6 +54,9 @@ export default function TaskForm({ taskCreated }) {
           className="text-black mb-8 px-2 py-1"
           type="text"
           value={selectedTask.title}
+          onChange={(e) =>
+            setSelectedTask({ ...setSelectedTask, title: e.target.value })
+          }
         />
         <button
           type="submit"
