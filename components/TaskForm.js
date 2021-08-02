ce2bc81827c00e6846c0a8c9043c5302c45a88c3
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-03 03:07:32 +0900

    フォームを空欄時の処理を追加

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -54,9 +54,13 @@ export default function TaskForm({ taskCreated }) {
           className="text-black mb-8 px-2 py-1"
           type="text"
           value={selectedTask.title}
-          onChange={(e) =>
-            setSelectedTask({ ...selectedTask, title: e.target.value })
-          }
+          onChange={(e) => {
+            if (e.target.value) {
+              setSelectedTask({ ...selectedTask, title: e.target.value });
+            } else {
+              setSelectedTask({ id: 0, title: "" });
+            }
+          }}
         />
         <button
           type="submit"
